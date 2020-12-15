f135a33ccb7019d5540eb3c88828ab1949d4b4d2
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-12-15 11:36:29 +0300

    fragments added

---
+++ app/src/main/java/com/alexis/e_shop/OnBoardingActivity.java
@@ -0,0 +1,54 @@
+package com.alexis.e_shop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    private FirebaseUser mCurrentUser;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        Button getStarted = findViewById(R.id.btn_getStarted);
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnBoardingActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mCurrentUser != null){
+            sendUserToHome();
+        }
+    }
+    public void sendUserToHome(){
+        Intent homeIntent = new Intent(OnBoardingActivity.this, MainActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(homeIntent);
+        finish();
+    }
+
+}
